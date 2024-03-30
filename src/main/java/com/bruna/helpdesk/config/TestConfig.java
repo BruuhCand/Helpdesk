73b6db6a6dc6f7718a490ca7e523ec6236edf11b
BruuhCand <brunacandidorosa@yahoo.com.br>
2024-03-30 15:34:29 -0300

    perfil test criado2

--- src/main/java/com/bruna/helpdesk/config/TestConfig.java
+++ src/main/java/com/bruna/helpdesk/config/TestConfig.java
@@ -1,10 +1,21 @@
 package com.bruna.helpdesk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.bruna.helpdesk.services.DBService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
-
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean //sempre que chamar a classe TestConfig chama esse método automático 
+	public void instanciaDB(){
+		this.dbService.instanciaDB();
+	}
 }
